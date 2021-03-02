c1756f1280077c8491ed7e6a3eb3a3f8cd54e22b
Maxim Doroshenko <m.s.doroshenko@gmail.com>
2021-03-02 18:46:33 +0300

    [final] Add inserts

---
+++ Final/insert_data.sql
@@ -0,0 +1,13 @@
+-- products
+
+INSERT INTO `max_oil`.`product` (`name`) VALUES ('ДТ');
+INSERT INTO `max_oil`.`product` (`name`) VALUES ('ДТ Зима');
+INSERT INTO `max_oil`.`product` (`name`) VALUES ('АИ-92');
+INSERT INTO `max_oil`.`product` (`name`) VALUES ('АИ-95');
+INSERT INTO `max_oil`.`product` (`name`) VALUES ('АИ-95 Турбо');
+INSERT INTO `max_oil`.`product` (`name`) VALUES ('АИ-98');
+INSERT INTO `max_oil`.`product` (`name`) VALUES ('АИ-98 Анлим');
+INSERT INTO `max_oil`.`product` (`name`) VALUES ('Американо');
+INSERT INTO `max_oil`.`product` (`name`) VALUES ('Капучино');
+INSERT INTO `max_oil`.`product` (`name`) VALUES ('Хотдог');
+
