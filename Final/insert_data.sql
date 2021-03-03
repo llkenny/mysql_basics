6efb3c46a531b89b078104ec02a6104ac9cc0921
Maxim Doroshenko <m.s.doroshenko@gmail.com>
2021-03-03 11:00:33 +0300

    [final] Add station data

--- Final/insert_data.sql
+++ Final/insert_data.sql
@@ -52,3 +52,25 @@ INSERT INTO `max_oil`.`organization` (`discount_multiplier`, `organization_detai
 INSERT INTO `max_oil`.`organization` (`discount_multiplier`, `organization_details_id`, `manager_id`) VALUES ('1', '9', '3');
 INSERT INTO `max_oil`.`organization` (`discount_multiplier`, `organization_details_id`, `manager_id`) VALUES ('1.01', '10', '4');
 
+-- station
+INSERT INTO `max_oil`.`station` (`address`, `latitude`, `longitude`, `active`, `partner_id`) VALUES ('Россия Московская область г.Сергиев-Посад Скобяное шоссе', '58.283834', '36.142853', '1', '1');
+INSERT INTO `max_oil`.`station` (`address`, `latitude`, `longitude`, `active`, `partner_id`) VALUES ('Белоруссия Гродненская область Лидинский район M-6, 159-й км, 1', '56.285134', '33.232853', '1', '1');
+INSERT INTO `max_oil`.`station` (`address`, `latitude`, `longitude`, `active`, `partner_id`) VALUES ('Свердловская область Билимбай Ленина', '54.285234', '36.133823', '1', '1');
+INSERT INTO `max_oil`.`station` (`address`, `latitude`, `longitude`, `active`, `partner_id`) VALUES ('Бресткая область Брест ул.Лейтенанта Рябцева', '55.281834', '38.132453', '1', '2');
+INSERT INTO `max_oil`.`station` (`address`, `latitude`, `longitude`, `active`, `partner_id`) VALUES ('Россия Калужская область Жиздринский р-н, 300 км трассы М-3 (справа)', '58.289934', '37.135853', '1', '2');
+INSERT INTO `max_oil`.`station` (`address`, `latitude`, `longitude`, `active`, `partner_id`) VALUES ('Россия ХМАО г. Нижневартовск ул. Интернациональная', '55.289714', '37.130953', '1', '2');
+INSERT INTO `max_oil`.`station` (`address`, `latitude`, `longitude`, `active`, `partner_id`) VALUES ('Россия Республика Мордовия с. Новое Зубарево Р-180, 91 км', '56.289124', '32.132153', '1', '2');
+INSERT INTO `max_oil`.`station` (`address`, `latitude`, `longitude`, `active`, `partner_id`) VALUES ('Россия Москва Каширское шоссе', '59.280334', '31.132143', '0', '3');
+INSERT INTO `max_oil`.`station` (`address`, `latitude`, `longitude`, `active`, `partner_id`) VALUES ('Россия Мурманская область Мончегорск Металлургов пр-т', '55.231234', '33.228853', '0', '3');
+INSERT INTO `max_oil`.`station` (`address`, `latitude`, `longitude`, `active`, `partner_id`) VALUES ('Россия Нижегородская область г. Нижний Новгород Долгополова', '59.208834', '32.102053', '1', '3');
+INSERT INTO `max_oil`.`station` (`address`, `latitude`, `longitude`, `active`, `partner_id`) VALUES ('Россия Челябинская область г. Кыштым ул. Ленина', '57.289124', '33.131353', '1', '4');
+INSERT INTO `max_oil`.`station` (`address`, `latitude`, `longitude`, `active`, `partner_id`) VALUES ('Россия Москва Москва Сормовский', '54.209834', '39.100353', '1', '4');
+INSERT INTO `max_oil`.`station` (`address`, `latitude`, `longitude`, `active`, `partner_id`) VALUES ('Россия Московская область Щелково Фрунзе', '56.289332', '32.139053', '1', '4');
+INSERT INTO `max_oil`.`station` (`address`, `latitude`, `longitude`, `active`, `partner_id`) VALUES ('Россия Свердловская область Талица ул. Луначарского', '58.289084', '34.131953', '1', '4');
+INSERT INTO `max_oil`.`station` (`address`, `latitude`, `longitude`, `active`, `partner_id`) VALUES ('Россия Пермский край Пермь Гашкова', '54.289834', '39.137153', '1', '2');
+INSERT INTO `max_oil`.`station` (`address`, `latitude`, `longitude`, `active`, `partner_id`) VALUES ('Россия Волгоградская область Урюпинск Репина', '55.287634', '37.131753', '0', '3');
+INSERT INTO `max_oil`.`station` (`address`, `latitude`, `longitude`, `active`, `partner_id`) VALUES ('Россия Волгоградская область Урюпинск Штеменко', '58.227834', '34.139853', '1', '1');
+INSERT INTO `max_oil`.`station` (`address`, `latitude`, `longitude`, `active`, `partner_id`) VALUES ('Россия Челябинская область г. Куса ул. 3 Интернационала', '53.285134', '37.135253', '1', '4');
+INSERT INTO `max_oil`.`station` (`address`, `latitude`, `longitude`, `active`, `partner_id`) VALUES ('Россия Нижегородская область с. Сеченово ул. Советская', '54.319834', '33.132723', '1', '2');
+INSERT INTO `max_oil`.`station` (`address`, `latitude`, `longitude`, `active`, `partner_id`) VALUES ('Россия Волгоградская область Новоаннинский Рабочая', '55.287634', '35.132913', '1', '1');
+INSERT INTO `max_oil`.`station` (`address`, `latitude`, `longitude`, `active`, `partner_id`) VALUES ('Россия Забайкальский край г. Чита', '59.289534', '36.132283', '1', '3');
