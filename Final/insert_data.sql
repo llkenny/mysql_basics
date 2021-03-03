aefb77639ccaaf91128f618625e3339daa0818ad
Maxim Doroshenko <m.s.doroshenko@gmail.com>
2021-03-03 20:26:27 +0300

    [final] Add cards data, add fill_card procedure, fix number unique

--- Final/insert_data.sql
+++ Final/insert_data.sql
@@ -170,3 +170,39 @@ INSERT INTO `virtual_card` (`device_id`, `type`, `created_at`, `expire_at`) VALU
 INSERT INTO `virtual_card` (`device_id`, `type`, `created_at`, `expire_at`) VALUES ('mbnsjkdfhsdkjfhbdsjkfhbdshjfgb', 'NFC', '2019-02-21 21:00:00', '2021-06-11 00:00:00');
 INSERT INTO `virtual_card` (`device_id`, `type`, `created_at`, `expire_at`) VALUES ('oiuhiuehrkjhbsdjhfbsdkjfhbsdjf', 'NFC', '2021-01-04 08:30:15', '2022-08-01 00:00:00');
 
+-- card
+
+DROP PROCEDURE IF EXISTS fill_card;
+
+DELIMITER //
+
+CREATE PROCEDURE fill_card(`count` INT)
+BEGIN
+
+	DECLARE i INT DEFAULT 1;
+	DECLARE virtual_card_id INT;
+	DECLARE card_employee_id INT;
+	DECLARE card_contract_id INT;
+	DECLARE card_number DOUBLE;
+    DECLARE card_active TINYINT;
+
+	START TRANSACTION;
+
+	SELECT COUNT(*) INTO @virtual_card_count from virtual_card;
+	SELECT COUNT(*) INTO @employee_count from employee;
+
+	WHILE i < `count` DO
+		SET i = i + 1;
+		SET virtual_card_id = FLOOR(RAND()*(@virtual_card_count)+1);
+		SET card_employee_id = FLOOR(RAND()*(@employee_count)+1);
+        SET card_number = 90000000000000 + RAND() * 1000000000000;
+        SET card_active = ROUND(RAND());
+		SELECT contract_id INTO card_contract_id FROM employee WHERE card_employee_id = id LIMIT 1;
+		INSERT IGNORE INTO `card` (`number`, `virtual_card_id`, `employee_id`, `contract_id`, `created_at`, `active`) VALUES (card_number, virtual_card_id, card_employee_id, card_contract_id, NOW(), card_active);
+	END WHILE;
+    
+	COMMIT;
+END//
+DELIMITER ;
+
+CALL fill_card(50);
