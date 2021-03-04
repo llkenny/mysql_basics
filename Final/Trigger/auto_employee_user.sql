c4a19b54e9dfe4540c0995d5cb7a1b0531a4dc52
Maxim Doroshenko <m.s.doroshenko@gmail.com>
2021-03-04 11:07:42 +0300

    [final] Update triggers

--- Final/Trigger/auto_employee_user.sql
+++ Final/Trigger/auto_employee_user.sql
@@ -5,7 +5,6 @@
 DROP TRIGGER IF EXISTS `employee_AFTER_INSERT`;
 
 DELIMITER $$
-USE `max_oil`$$
 CREATE DEFINER = CURRENT_USER TRIGGER `employee_AFTER_INSERT` AFTER INSERT ON `employee` FOR EACH ROW
 BEGIN
 	INSERT INTO `user` (`username`, `employee_id`) VALUES (CONCAT('auto_', new.`id`), new.`id`);
--- Final/Trigger/employee_contract_organization_update_check.sql
+++ Final/Trigger/employee_contract_organization_update_check.sql
@@ -1,8 +1,16 @@
-CREATE DEFINER = CURRENT_USER TRIGGER `max_oil`.`employee_BEFORE_UPDATE` BEFORE UPDATE ON `employee` FOR EACH ROW
+/**
+Защита от создания пользователя с контрактом, не принадлежащим организации.
+*/
+
+DROP TRIGGER IF EXISTS `employee_BEFORE_UPDATE`;
+
+DELIMITER $$
+CREATE DEFINER = CURRENT_USER TRIGGER `employee_BEFORE_UPDATE` BEFORE UPDATE ON `employee` FOR EACH ROW
 BEGIN
 	DECLARE org_id INT;
     SELECT id INTO org_id FROM contract WHERE id = NEW.contract_id AND organization_id = NEW.organization_id LIMIT 1;
 	IF ISNULL(org_id) THEN
 		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Invalid contract for the organization';
     END IF;
-END
+END$$
+DELIMITER ;
